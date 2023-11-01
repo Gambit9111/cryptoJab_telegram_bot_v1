0563fda046c1a2d55ff1d6e835a147e0c724189f
Gambit9111 <bitgam911@gmail.com>
2023-11-01 06:03:11 -0400

    a

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ load_dotenv()
 TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
 
 # init telegram bot
-telegram_bot = telebot.TeleBot(TELEGRAM_BOT_API_KEY, parse_mode="HTML")
+telegram_bot = telebot.TeleBot(TELEGRAM_BOT_API_KEY, parse_mode="HTML", num_threads=5)
 
 @telegram_bot.message_handler(commands=['start'])
 def send_welcome(message):
     telegram_bot.reply_to(message, "Howdy, how are you doing?")
     print(message)
 
-telegram_bot.polling()
+def run():
+    telegram_bot.polling()
+
+if __name__ == "__main__":
+    run()
