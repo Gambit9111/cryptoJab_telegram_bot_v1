58237ee8821441f6f27ac9bba1c2b18b80349640
Gambit9111 <bitgam911@gmail.com>
2023-11-06 00:17:49 -0500

    changes to coinbase checkout handler

--- coinbase_checkout_handler.py
+++ coinbase_checkout_handler.py
@@ -6,30 +6,47 @@ load_dotenv()
 
 # initialize variables
 coinbase_api_key = os.getenv("COINBASE_API_KEY")
-coinbase_sub_30_days = os.getenv("COINBASE_SUB_30_DAYS")
-coinbase_sub_90_days = os.getenv("COINBASE_SUB_90_DAYS")
-coinbase_sub_365_days = os.getenv("COINBASE_SUB_365_DAYS")
+
+# ? 30 Days | 0.15 USD
+coinbase_sub_option_1_duration = os.getenv("COINBASE_SUB_OPTION_1_DURATION")
+coinbase_sub_option_1_price = os.getenv("COINBASE_SUB_OPTION_1_PRICE")
+
+# ? 90 Days | 0.20 USD
+coinbase_sub_option_2_duration = os.getenv("COINBASE_SUB_OPTION_2_DURATION")
+coinbase_sub_option_2_price = os.getenv("COINBASE_SUB_OPTION_2_PRICE")
+
+# ? 365 Days | 0.25 USD
+coinbase_sub_option_3_duration = os.getenv("COINBASE_SUB_OPTION_3_DURATION")
+coinbase_sub_option_3_price = os.getenv("COINBASE_SUB_OPTION_3_PRICE")
+
 coinbase_base_url = "https://commerce.coinbase.com/checkout/"
 coinbase_client = Client(api_key=coinbase_api_key)
 
 def create_coinbase_checkout_session(user_telegram_id: str, product_id: int):
 
     coinbase_sub_duration = None
+    coinbase_sub_price = None
 
     # match product_id with coinbase_product_id
     match product_id:
         case 0:
-            coinbase_sub_duration = coinbase_sub_30_days
+            # ? 30 days
+            coinbase_sub_duration = coinbase_sub_option_1_duration
+            coinbase_sub_price = coinbase_sub_option_1_price
         case 1:
-            coinbase_sub_duration = coinbase_sub_90_days
+            # ? 90 days
+            coinbase_sub_duration = coinbase_sub_option_2_duration
+            coinbase_sub_price = coinbase_sub_option_2_price
         case 2:
-            coinbase_sub_duration = coinbase_sub_365_days
+            # ? 365 days
+            coinbase_sub_duration = coinbase_sub_option_3_duration
+            coinbase_sub_price = coinbase_sub_option_3_price
     
     coinbase_checkout_info = {
         "name": f"Subscription for {coinbase_sub_duration} days",
         "description": user_telegram_id,
         "local_price": {
-            "amount": coinbase_sub_duration,
+            "amount": coinbase_sub_price,
             "currency": "USD"
         },
         "pricing_type": "fixed_price",
